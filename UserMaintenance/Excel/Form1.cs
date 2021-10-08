0abde48eed1cc67d10720aaa14b8677c71108e6a
Péter Erdei <Peter.Erdei@bitclub.hu>
2021-10-08 22:48:50 +0200

    CreateExcel

--- UserMaintenance/Excel/Form1.cs
+++ UserMaintenance/Excel/Form1.cs
@@ -7,14 +7,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ExcelR = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace Excel
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
+      
+        ExcelR.Application xlApp; 
+        ExcelR.Workbook xlWB; 
+        ExcelR.Worksheet xlSheet; 
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void LoadData()
+        {
+            Flats = context.Flat.ToList();
+        }
+
+        public void CreateExcel()
+        {
+            try
+            {
+                xlApp = new ExcelR.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) 
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+               
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
         }
     }
 }
