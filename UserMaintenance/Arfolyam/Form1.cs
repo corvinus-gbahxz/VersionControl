034aa430389d7f6f0c62a47ef21f0d7fe99b24a4
Péter Erdei <Peter.Erdei@bitclub.hu>
2021-10-24 19:43:21 +0200

    7.felaadtig

--- UserMaintenance/Arfolyam/Form1.cs
+++ UserMaintenance/Arfolyam/Form1.cs
@@ -10,35 +10,35 @@ using System.Windows.Forms;
 using Arfolyam.MnbServiceReference;
 using Arfolyam.Entities;
 using System.Xml;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Arfolyam
 {
     public partial class Form1 : Form
     {
         BindingList<RateData> Rates = new BindingList<RateData>();
+        BindingList<string> Currencies = new BindingList<string>();
 
         public Form1()
         {
             InitializeComponent();
-            CallWebService();
-            dataGridView1.DataSource = Rates;
-
         }
 
-        private void CallWebService()
+        private string CallWebService()
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
 
             var request = new GetExchangeRatesRequestBody()
             {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
+                currencyNames = comboBox1.Text,
+                startDate = dateTimePicker1.Value.ToString(),
+                endDate = dateTimePicker2.Value.ToString()
             };
 
             var response = mnbService.GetExchangeRates(request);
 
             var result = response.GetExchangeRatesResult;
+            return result;
         }
 
         private void ProcXML(string result)
@@ -61,5 +61,46 @@ namespace Arfolyam
             }
         }
 
+        private void PlotData()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+        }
+
+        private void RefreshData()
+        {
+            Rates.Clear();
+            ProcXML(CallWebService());
+            dataGridView1.DataSource = Rates;
+            PlotData();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
     }
 }
