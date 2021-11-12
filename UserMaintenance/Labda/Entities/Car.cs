d1b9d304a1afe82bf7e65273d5d2584d77f7b0b5
Péter Erdei <Peter.Erdei@bitclub.hu>
2021-11-12 16:18:56 +0100

    Car

--- UserMaintenance/Labda/Entities/Car.cs
+++ UserMaintenance/Labda/Entities/Car.cs
@@ -1,19 +1,19 @@
-﻿using System;
+﻿using Labda.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
-using Labda.Abstractions;
 
 namespace Labda.Entities
 {
-    class Ball : Toy
+    class Car : Toy
     {
         protected override void DrawImage(Graphics g)
         {
-            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
         }
     }
 }
